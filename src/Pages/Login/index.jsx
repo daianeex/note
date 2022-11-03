26c86dbedf3255362fd081d63a806384d41b8497
Daiane Gonçalves <daianeecs@icloud.com>
2022-11-03 18:14:43 -0300

    button logar

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -2,26 +2,45 @@ import { async } from "@firebase/util";
 import React from "react";
 import Auth from "../../lib/auth";
 import Button from "../../components/button";
-import { googleLogar } from "../../lib/auth"
+import { googleLogar, userLogin } from "../../lib/auth"
+import Input from "../../components/input";
+import { useNavigate } from "react-router-dom";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({onReceiveGoogle}) => {
+//const navigate = useNavigate()
 
-    const actionLoginGoogle = async () => {
-        let result = await Auth.googleLogar();
-
-        if(result){
+    const actionLoginGoogle = async (e) => {
+        e.preventDefault();
+        googleLogar()
+        .then(() => {
             alert("Entrou");
-        }else{
+        })
+        .catch((error) => {
             alert("Error");
-        }
+        })
     }
+
+    const actionLogin = async (e) => {
+        e.preventDefault();
+        userLogin()
+        .then(() => {
+            alert("Entrou");
+        })
+        .catch((error) => {
+            alert("Error");
+        })
+    }
+    
     return (
-    <>
+        <main className='headerLogin'>
         <h1>Entrar na sua conta</h1>
-        <input id="email" name="email" required="required" type="email" placeholder="E-mail"/> 
-        <input id="password" name="password" required="required" type="password" placeholder="Senha"/>
-        <p>Não tem conta? Criar</p>
-        <Button onClick={googleLogar}>Logar</Button>
-    </>
+        <Input id="email" name="email" required="required" type="email" placeholder="E-mail"/> <br />
+        <Input id="password" name="password" required="required" type="password" placeholder="Senha"/>
+        <Button onClick={actionLogin}>Entrar</Button>
+        <p>Esqueceu sua senha?</p>
+        <Button onClick={actionLoginGoogle}>Google</Button>
+        <p>Não tem conta? <a href="/register" /></p>
+        
+    </main>
     )
     }
